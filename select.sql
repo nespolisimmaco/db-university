af73e0f69249da4166ebd86949065a1a6e254660
nespolisimmaco <snespoli@yahoo.com>
2023-05-12 20:03:18 +0200

    Query numero 3 e 4, modificata query 1

--- select.sql
+++ select.sql
@@ -1,6 +1,6 @@
 
 1. Selezionare tutti gli studenti nati nel 1990 (160)
-SELECT *
+SELECT `name`, `surname`, `date_of_birth`
 FROM `students`
 WHERE YEAR(`date_of_birth`) = 1990;
 
@@ -10,11 +10,16 @@ FROM `courses`
 WHERE `cfu` > 10;
 
 3. Selezionare tutti gli studenti che hanno più di 30 anni
-
+SELECT `name`, `surname`, `date_of_birth`
+FROM `students`
+WHERE DATE_FORMAT(FROM_DAYS(DATEDIFF(NOW(),`date_of_birth`)), '%Y') + 0 > 30;
 
 4. Selezionare tutti i corsi del primo semestre del primo anno di un qualsiasi corso di
 laurea (286)
-
+SELECT `degree_id`, `name`, `period`, `year`
+FROM `courses`
+WHERE `period` = 'I semestre'
+AND `year` = 1;
 
 5. Selezionare tutti gli appelli d'esame che avvengono nel pomeriggio (dopo le 14) del
 20/06/2020 (21)
